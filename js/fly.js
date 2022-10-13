022d620510c2ce2c9c3073fe098774d4e9e300e9
hyoga-dev <75929898+hyoga-dev@users.noreply.github.com>
2022-10-13 16:38:31 +0700

    cleaning

--- js/fly.js
+++ js/fly.js
@@ -1,120 +1,158 @@
+const bix = document.querySelectorAll(".box");
 
-// const fltd = 10;
 let a, b, rectLeft, rectTop, prevEvent, currentEvent;
+// const bix = document.querySelectorAll(".box");y
 
 
-// * FlyUp
-export default function flyUp(e) {
-  a = e.clientX;
-  b = e.clientY;
+export default function udin() {
+
+
+for (let i = 0; i < bix.length; i++ ) {
   
-  rectLeft = Math.floor(box.getBoundingClientRect().left);
-  rectTop = Math.floor(box.getBoundingClientRect().top);
-  currentEvent = e;
-
-  box.style.cursor = "grabbing";
-  box.style.boxShadow = "0 30px 50px 0 rgba(0, 0, 0, 0.19)";
-
-  window.addEventListener("mousemove", fly);
-  window.addEventListener("mouseup", flyDown);
-} 
+  let box = bix[i]
+  // console.log(box)  
+  
+  
+  box.addEventListener("mousedown", flyUp); // flying card
 
 
-
-// * FlyDown
-function flyDown() {
-  box.style.cursor = "grab";
-  box.style.boxShadow = "none";
-
-  window.removeEventListener("mousemove", fly);
-  window.removeEventListener("mouseup", flyDown);
-
-  const top = parseInt(getComputedStyle(box).getPropertyValue('top')),
-        left = parseInt(getComputedStyle(box).getPropertyValue('left')) ,
-        container = document.querySelector(".container"),
-        boxWidth = parseInt(getComputedStyle(box).getPropertyValue('width')), 
-        boxHeight = parseInt(getComputedStyle(box).getPropertyValue('height')), 
-        conHeight = parseInt(getComputedStyle(container).getPropertyValue("height")),
-        conWidth = parseInt(getComputedStyle(container).getPropertyValue("width"));
-
-  if (top < 0 &&  left > conWidth - boxWidth) {
-    box.style.top = `0`;
-    box.style.left = `${conWidth - boxWidth}px`;
-    return
-
-  } else if (top > conHeight - boxHeight &&  left < 0) {
-    box.style.top = `${conHeight - boxHeight}px`;
-    box.style.left = `0`;
-    console.log(2)
-    return
-
-  } else if (top > conHeight - boxHeight &&  left > conWidth - boxWidth) {
-    box.style.top = `${conHeight - boxHeight}px`;
-    box.style.left = `${conWidth - boxWidth}px`;
-    console.log(2)
-    return
-
-  } else if (top < 0 && left < 0) {
-    box.style.top = "0";
-    box.style.left = "0";
-    return
-
-  } else if ( top < 0) {
-    box.style.top = "0" 
-    return
-
-  } else if (left < 0) {
-    box.style.left = "0"
-    return
-
-  } else if (top > conHeight - boxHeight) {
-    box.style.top = `${conHeight - boxHeight}px` 
-    return
-
-  } else if (left > conWidth - boxWidth) {
-    box.style.left = `${conWidth - boxWidth}px` 
-    return
-  }
-}
-
-
-
-// * Fly
-function fly(e) {
-  const mouseHor = e.clientX,
-        mouseVer = e.clientY;
-
-  box.style.left = `${mouseHor - (a - rectLeft)}px`;
-  box.style.top = `${mouseVer - (b - rectTop)}px`;
-
-  // * Wosh
-  function wosh() {
-
-    currentEvent = e;
-    if (prevEvent && currentEvent) {
-      var movementX = Math.floor(currentEvent.screenX - prevEvent.screenX);
-      var movementY = Math.floor(currentEvent.screenY - prevEvent.screenY);
-
-      const mx = movementX * 1.5;
-      const my = movementY * -1.5;
-      if (mx < 40 && mx > -40 && my < 40 && my > -40) {
-        box.style.transform = `rotateX(${my / 1.2}deg) rotateY(${mx / 1.2}deg)`;
-      } 
+// * FlyDown =================================================================
+  function flyUp(e) {
+    let index = parseInt(getComputedStyle(box).getPropertyValue("z-index"))
+    a = e.clientX;
+    b = e.clientY;
+    
+    let arr = []
+    
+    for (let j = 0; j < bix.length; j++) {
+      let bux = bix[j]
+      index = parseInt(getComputedStyle(bux).getPropertyValue("z-index"))
+      arr.push(index)
     }
 
-    document.getElementById("demo").innerText = "movement Y : " + movementX;
-    document.getElementById("demo2").innerText = "movement X : " + movementY;
-    // const top = getComputedStyle(box).getPropertyValue('top')
-    // const left = getComputedStyle(box).getPropertyValue('left')
-    
-    console.log("Y is : ", movementY)
-    console.log("X is : ", movementX)
+    let max = Math.max( ...arr )
+    // console.log(max)
 
-    // document.getElementById("demo").innerText = left;
-    // document.getElementById("demo2").innerText = "Top is: " + top;
+    rectLeft = Math.floor(box.getBoundingClientRect().left);
+    rectTop = Math.floor(box.getBoundingClientRect().top);
+  
+    box.style.zIndex = max + 1; 
+    document.getElementById("demo5").innerHTML = "Arr : " + arr;
 
-    prevEvent = currentEvent;
+    box.style.cursor = "grabbing";
+    box.style.boxShadow = "0 30px 50px 0 rgba(0, 0, 0, 0.19)";
+  
+    window.addEventListener("mousemove", fly);
+    window.addEventListener("mouseup", flyDown);
+  } 
+  
+
+
+
+
+
+
+   // * Fly =================================================================
+   function fly(e) {
+    const mouseHor = e.clientX,
+          mouseVer = e.clientY;
+  
+    box.style.left = `${mouseHor - (a - rectLeft)}px`;
+    box.style.top = `${mouseVer - (b - rectTop)}px`;
+  
+    // * Wosh
+    function wosh() {
+  
+      currentEvent = e;
+      if (prevEvent && currentEvent) {
+        var movementX = Math.floor(currentEvent.screenX - prevEvent.screenX);
+        var movementY = Math.floor(currentEvent.screenY - prevEvent.screenY);
+  
+        const mx = movementX * 1.5;
+        const my = movementY * -1.5;
+        if (mx < 40 && mx > -40 && my < 40 && my > -40) {
+          box.style.transform = `rotateX(${my / 1.2}deg) rotateY(${mx / 1.2}deg)`;
+        } 
+      }
+
+      prevEvent = currentEvent;
+    }
+    wosh();
   }
-  wosh();
-}
+  
 
+
+
+
+
+
+  // * FlyDown =================================================================
+  function flyDown() {
+    
+    box.style.cursor = "grab";
+    box.style.boxShadow = "none";
+    let index1 = getComputedStyle(bix[0]).getPropertyValue("z-index")
+    let index2 = getComputedStyle(bix[1]).getPropertyValue("z-index")
+    let index3 = getComputedStyle(bix[2]).getPropertyValue("z-index")
+    let index4 = getComputedStyle(bix[3]).getPropertyValue("z-index")
+
+    document.getElementById("demo").innerHTML = "Box number : " + 0 + ", have index of: " + index1;
+    document.getElementById("demo2").innerHTML = "Box number : " + 1 + ", have index of: " + index2;
+    document.getElementById("demo3").innerHTML = "Box number : " + 2 + ", have index of: " + index3;
+    document.getElementById("demo4").innerHTML = "Box number : " + 3 + ", have index of: " + index4;
+
+    // box.style.zIndex = "auto"; 
+
+  
+    window.removeEventListener("mousemove", fly);
+    window.removeEventListener("mouseup", flyDown);
+  
+    const top = parseInt(getComputedStyle(box).getPropertyValue('top')),
+          left = parseInt(getComputedStyle(box).getPropertyValue('left')) ,
+          container = document.querySelector(".container"),
+          boxWidth = parseInt(getComputedStyle(box).getPropertyValue('width')), 
+          boxHeight = parseInt(getComputedStyle(box).getPropertyValue('height')), 
+          conHeight = parseInt(getComputedStyle(container).getPropertyValue("height")),
+          conWidth = parseInt(getComputedStyle(container).getPropertyValue("width"));
+  
+    if (top < 0 &&  left > conWidth - boxWidth) {
+      box.style.top = `0`;
+      box.style.left = `${conWidth - boxWidth}px`;
+      return
+  
+    } else if (top > conHeight - boxHeight &&  left < 0) {
+      box.style.top = `${conHeight - boxHeight}px`;
+      box.style.left = `0`;
+      console.log(2)
+      return
+  
+    } else if (top > conHeight - boxHeight &&  left > conWidth - boxWidth) {
+      box.style.top = `${conHeight - boxHeight}px`;
+      box.style.left = `${conWidth - boxWidth}px`;
+      console.log(2)
+      return
+  
+    } else if (top < 0 && left < 0) {
+      box.style.top = "0";
+      box.style.left = "0";
+      return
+  
+    } else if ( top < 0) {
+      box.style.top = "0" 
+      return
+  
+    } else if (left < 0) {
+      box.style.left = "0"
+      return
+  
+    } else if (top > conHeight - boxHeight) {
+      box.style.top = `${conHeight - boxHeight}px` 
+      return
+  
+    } else if (left > conWidth - boxWidth) {
+      box.style.left = `${conWidth - boxWidth}px` 
+      return
+    }
+  }
+}
+}
--- main.js
+++ main.js
@@ -3,49 +3,42 @@
 // import fly from "./js/fly.js";
 
 // init
+// const btn = document.getElementById("btn")
 const bix = document.querySelectorAll(".box");
 let a, b, rectLeft, rectTop, prevEvent, currentEvent;
 
-// even handler
 
 
+// main
 for (let i = 0; i < bix.length; i++ ) {
   
   let box = bix[i]
-  // console.log(box)  
-  
-  
   box.addEventListener("mousedown", flyUp); // flying card
-  // box.innerHTML = "Bambang";
-  
 
 
-
-
-
-// * FlyDown =================================================================
+// * FlyUp =================================================================
   function flyUp(e) {
     let index = parseInt(getComputedStyle(box).getPropertyValue("z-index"))
     a = e.clientX;
     b = e.clientY;
     
-    let text = ""
+    let arr = []
     
     for (let j = 0; j < bix.length; j++) {
-  
       let bux = bix[j]
       index = parseInt(getComputedStyle(bux).getPropertyValue("z-index"))
-      console.log(index)
-      
+      arr.push(index)
     }
+
+    let max = Math.max( ...arr )
+    // console.log(max)
 
     rectLeft = Math.floor(box.getBoundingClientRect().left);
     rectTop = Math.floor(box.getBoundingClientRect().top);
-    // currentEvent = e;
   
-    document.getElementById("demo2").innerHTML = "Fly : " + text;
+    box.style.zIndex = max + 1; 
+    document.getElementById("demo5").innerHTML = "Arr : " + arr;
 
-    box.style.zIndex = index + 1; 
     box.style.cursor = "grabbing";
     box.style.boxShadow = "0 30px 50px 0 rgba(0, 0, 0, 0.19)";
   
@@ -53,13 +46,7 @@ for (let i = 0; i < bix.length; i++ ) {
     window.addEventListener("mouseup", flyDown);
   } 
   
-
-
-
-
-
-
-   // * Fly =================================================================
+// * Fly =================================================================
    function fly(e) {
     const mouseHor = e.clientX,
           mouseVer = e.clientY;
@@ -87,22 +74,20 @@ for (let i = 0; i < bix.length; i++ ) {
     wosh();
   }
   
-
-
-
-
-
-
-  // * FlyDown =================================================================
+// * FlyDown =================================================================
   function flyDown() {
+    
     box.style.cursor = "grab";
     box.style.boxShadow = "none";
-    let index = getComputedStyle(box).getPropertyValue("z-index")
+    let index1 = getComputedStyle(bix[0]).getPropertyValue("z-index")
+    let index2 = getComputedStyle(bix[1]).getPropertyValue("z-index")
+    let index3 = getComputedStyle(bix[2]).getPropertyValue("z-index")
+    let index4 = getComputedStyle(bix[3]).getPropertyValue("z-index")
 
-    document.getElementById("demo").innerHTML = "Drop : " + index;
-
-    // box.style.zIndex = "auto"; 
-
+    document.getElementById("demo").innerHTML = "Box number : " + 0 + ", have index of: " + index1;
+    document.getElementById("demo2").innerHTML = "Box number : " + 1 + ", have index of: " + index2;
+    document.getElementById("demo3").innerHTML = "Box number : " + 2 + ", have index of: " + index3;
+    document.getElementById("demo4").innerHTML = "Box number : " + 3 + ", have index of: " + index4;
   
     window.removeEventListener("mousemove", fly);
     window.removeEventListener("mouseup", flyDown);
