d920cdbaed655416d2c1d3c6c3bed4e9abd925e7
hyoga-dev <75929898+hyoga-dev@users.noreply.github.com>
2022-11-24 16:57:23 +0700

    fix unselect

--- src/js/contextmenu/eventHandler.js
+++ src/js/contextmenu/eventHandler.js
@@ -1,5 +1,7 @@
 
 export default function eventHandler() {
+  
+  document.addEventListener("mousedown", this.unselect)
   document.addEventListener("contextmenu", e => e.preventDefault())
   document.addEventListener("mousedown", e => this.addContext(e))
   document.activeElement.addEventListener("keydown", e => this.shortCut(e))
